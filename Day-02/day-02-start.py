370104bbbeae1eb0accc3e22ae81a2fb726eea61
MarylinV <98238827+VMarylin@users.noreply.github.com>
2022-01-25 21:30:46 +0000

    learning data types and type casting

--- Day-02/day-02-start.py
+++ Day-02/day-02-start.py
@@ -1,4 +1,26 @@
 #Data Types
 
-#String - subscripting
-print("Hello"[0])
+#Data Types
+
+#string
+print("Hello" [0])
+print("Hello" [4])
+
+
+#integer
+print(123 + 345)
+
+print(123_456_789)
+
+#float
+3.14159
+
+#boolean
+ 
+True
+False
+num_char = len(input("What is your name?\n"))
+new_num_char = str(num_char) #type casting
+print("Your name has " + new_num_char + " characters.")
+
+print(type(num_char))
