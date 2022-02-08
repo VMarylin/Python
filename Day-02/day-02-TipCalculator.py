6d882094542cfcf29f44a2582eb9a1c210c7cc14
MarylinV <98238827+VMarylin@users.noreply.github.com>
2022-02-08 19:17:53 +0000

    updated the tip percemnt

--- Day-02/day-02-TipCalculator.py
+++ Day-02/day-02-TipCalculator.py
@@ -1,7 +1,7 @@
 #If the bill was $150.00, split between 5 people, with 12% tip. 
 print("Welcome to the tip calculator!")
 bill = input("What was the total bill? $")
-tip_percent = input("What percentage tip would you like to give? 10%, 12% or 15%? ")
+tip_percent = input("What percentage tip would you like to give? 10, 12 or 15? ")
 no_of_people = input("How many people to split the bill? ")
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 tip = (int(tip_percent)/100) + 1
